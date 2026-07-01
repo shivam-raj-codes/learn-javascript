1bcd4eb7b0311e452096574172176f5c7f4036d0
popeye-codes <hey.shivam.uno@gmail.com>
2026-07-01 06:45:01 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer - version
+
+// alert(3 + 3) // we'r using Nodejs, not browser , Also alert need NO Semicolon(;)
+
+console.log(3 + 3); 
+
+
+let name = "shiv";
+let age = 18;
+
+let isLoggedIn = false;
+
+// ----- dataTypes ----
+
+// number => 2 to power 32(range)
+
+// bigint
+// string => ""
+// boolean => true / false
+// 🌟 null => standAlone value
+
+// undefined => 
+
+// symbol => used for uniqueness
+
+// object
+
+console.log(typeof("age"));
+
+console.log(typeof(null)); // => object
+
+console.log(undefined); // => undefined , bcoz its a type in js
+
